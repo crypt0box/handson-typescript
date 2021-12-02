34c8edfa07f07f46c0cb7881b29feac9563da20c
crypt0box <pray.happy76@gmail.com>
2021-12-02 17:14:27 +0900

    refacotor: code refactoring

--- dist/index.js
+++ dist/index.js
@@ -35,6 +35,14 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var HintAndBlow = /** @class */ (function () {
+    function HintAndBlow() {
+        this.answerSource = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        this.answer = [];
+        this.tryCount = 0;
+    }
+    return HintAndBlow;
+}());
 var printLine = function (text, breakLine) {
     if (breakLine === void 0) { breakLine = true; }
     process.stdout.write(text + (breakLine ? "\n" : ""));
@@ -55,7 +63,7 @@ var promptInput = function (text) { return __awaiter(void 0, void 0, void 0, fun
     });
 }); };
 (function () { return __awaiter(void 0, void 0, void 0, function () {
-    var name, age;
+    var name, age, hintAndBlow;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0: return [4 /*yield*/, promptInput('名前を入力してください')];
@@ -67,6 +75,7 @@ var promptInput = function (text) { return __awaiter(void 0, void 0, void 0, fun
                 age = _a.sent();
                 console.log(age);
                 process.exit();
+                hintAndBlow = new HintAndBlow();
                 return [2 /*return*/];
         }
     });
