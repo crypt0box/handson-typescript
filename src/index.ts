0a4005e8dfabc15ebe679a6fc037702be8d40751
crypt0box <pray.happy76@gmail.com>
2021-12-02 17:39:10 +0900

    feat: implement check method

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,28 @@ class HintAndBlow {
       }
     }
   }
+
   async play() {
     const inputArr = (await promptInput('「,」区切りで３つの数字を入力してください')).split(',')
+    const result = this.check(inputArr)
+  }
+
+  check(input: string[]) {
+    let hitCount = 0
+    let blowCount = 0
+
+    input.forEach((val, index) => {
+      if (val === this.answer[index]) {
+        hitCount += 1
+      } else if (this.answer.includes(val)) {
+        blowCount += 1
+      }
+    })
+
+    return {
+      hit: hitCount,
+      blow: blowCount
+    }
   }
 }
 
