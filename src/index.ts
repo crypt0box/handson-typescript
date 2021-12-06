92f898b4b4acd54596dfeef6b8ffecdef02fac56
crypt0box <pray.happy76@gmail.com>
2021-12-06 21:52:59 +0900

    reafactor: add private and readonly qualifier

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 class HintAndBlow {
-  answerSource = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  answer: string[] = []
-  tryCount = 0
+  private readonly answerSource = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  private answer: string[] = []
+  private tryCount = 0
   setting() {
     const answerLength = 3
 
@@ -29,7 +29,7 @@ class HintAndBlow {
     }
   }
 
-  check(input: string[]) {
+  private check(input: string[]) {
     let hitCount = 0
     let blowCount = 0
 
