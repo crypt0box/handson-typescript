02cfd19e3a0f031c91fe7392e8422437e531b502
crypt0box <pray.happy76@gmail.com>
2021-12-06 22:11:09 +0900

    feat: implement mode feature

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,104 @@
 class HintAndBlow {
-  private readonly answerSource = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  private answer: string[] = []
-  private tryCount = 0
+  private readonly answerSource = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  private answer: string[] = [];
+  private tryCount = 0;
+  private mode: "normal" | "hard";
+
+  constructor(mode: "normal" | "hard") {
+    this.mode = mode;
+  }
+
   setting() {
-    const answerLength = 3
+    const answerLength = this.getAnswerLength();
 
     while (this.answer.length < answerLength) {
-      const randNum = Math.floor(Math.random() * this.answerSource.length)
-      const selectedItem = this.answerSource[randNum]
+      const randNum = Math.floor(Math.random() * this.answerSource.length);
+      const selectedItem = this.answerSource[randNum];
       if (!this.answer.includes(selectedItem)) {
-        this.answer.push(selectedItem)
+        this.answer.push(selectedItem);
       }
     }
   }
 
   async play() {
-    const inputArr = (await promptInput('「,」区切りで３つの数字を入力してください')).split(',')
+    const answerLength = this.getAnswerLength();
+    const inputArr = (
+      await promptInput(
+        `「,」区切りで${answerLength}つの数字を入力してください`
+      )
+    ).split(",");
 
     if (!this.validate(inputArr)) {
-      printLine('無効な入力です。')
-      await this.play()
-      return
+      printLine("無効な入力です。");
+      await this.play();
+      return;
     }
-    
-    const result = this.check(inputArr)
+
+    const result = this.check(inputArr);
 
     if (result.hit !== this.answer.length) {
       // 不正解だったら続ける
-      printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`)
-      this.tryCount += 1
-      await this.play()
+      printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`);
+      this.tryCount += 1;
+      await this.play();
     } else {
       // 正解だったら終了
-      this.tryCount += 1
+      this.tryCount += 1;
     }
   }
 
   private check(input: string[]) {
-    let hitCount = 0
-    let blowCount = 0
+    let hitCount = 0;
+    let blowCount = 0;
 
     input.forEach((val, index) => {
       if (val === this.answer[index]) {
-        hitCount += 1
+        hitCount += 1;
       } else if (this.answer.includes(val)) {
-        blowCount += 1
+        blowCount += 1;
       }
-    })
+    });
 
     return {
       hit: hitCount,
-      blow: blowCount
-    }
+      blow: blowCount,
+    };
   }
 
   end() {
-    printLine(`正解です！ \n試行回数： ${this.tryCount}回`)
-    process.exit()
+    printLine(`正解です！ \n試行回数： ${this.tryCount}回`);
+    process.exit();
   }
 
   private validate(inputArr: string[]) {
-    const isLengthValid = inputArr.length === this.answer.length
-    const isAllAnswerSourceOption = inputArr.every((val) => this.answerSource.includes(val))
-    const isAllDifferentValues = inputArr.every((val, i) => inputArr.indexOf(val) === i)
-    return isLengthValid && isAllAnswerSourceOption && isAllDifferentValues
+    const isLengthValid = inputArr.length === this.answer.length;
+    const isAllAnswerSourceOption = inputArr.every((val) =>
+      this.answerSource.includes(val)
+    );
+    const isAllDifferentValues = inputArr.every(
+      (val, i) => inputArr.indexOf(val) === i
+    );
+    return isLengthValid && isAllAnswerSourceOption && isAllDifferentValues;
+  }
+
+  private getAnswerLength() {
+    switch (this.mode) {
+      case "normal":
+        return 3;
+      case "hard":
+        return 4;
+    }
   }
 }
 
@@ -80,8 +115,8 @@ const promptInput = async (text: string) => {
 };
 
 (async () => {
-  const hintAndBlow = new HintAndBlow()
-  hintAndBlow.setting()
-  await hintAndBlow.play()
-  hintAndBlow.end()
-})()
+  const hintAndBlow = new HintAndBlow("normal");
+  hintAndBlow.setting();
+  await hintAndBlow.play();
+  hintAndBlow.end();
+})();
